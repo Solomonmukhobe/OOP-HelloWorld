aec16c22748c7d96b3db038aedbb5024a325550c
Solomonmukhobe <solomonmukhobe@gmail.com>
2025-01-23 22:06:30 +0300

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+AUTHOR: SOLOMON MUKHOBE
+REG NO: CT101/G/19976/23
+DATE: 23/01/2025
+A PROGRAM TO CALCULATE EMPLOYEE BONUS
+ */
+//MAIN CLASS
+public class HelloWorld {
+    //MAIN METHOD
+    public static void main(String[] args)
+    {
+        //Prints out Hello world
+          System.out.print("Hello World");
+    }
